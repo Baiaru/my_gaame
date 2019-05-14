b791e1d06dfd33ee3f3fd741ea2df5fe70472300
Baiaru <44421288+Baiaru@users.noreply.github.com>
2019-05-14 13:43:44 +0700

    Update jump.py

--- jump.py
+++ jump.py
@@ -10,7 +10,7 @@ display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('JUMP!')
 icon = pygame.image.load('man.png')
 pygame.display.set_icon(icon)
-pygame.mixer.music.load('music/fon_music.mp3')
+pygame.mixer.music.load('fon_music.mp3')
 pygame.mixer.music.play()
 
 stone_img = [pygame.image.load('stone1.png'), pygame.image.load('stone2.png'), pygame.image.load('stone3.png')]
